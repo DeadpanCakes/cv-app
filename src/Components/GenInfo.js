5a53745c010eb4e39688a318e52d14d318b83560
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-02-19 10:25:40 -0800

    changed GenInfo.js to be stateful class

--- src/Components/GenInfo.js
+++ src/Components/GenInfo.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import GenInfoForm from "./GenInfoForm";
 
-const GeneralInfo = () => {
-  return (
-    <div>
-      <h1>General Information</h1>
-      <GenInfoForm />
-    </div>
-  );
-};
+class GeneralInfo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      lastName: "",
+      phoneNumber: "",
+      email: "",
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>General Information</h1>
+        <GenInfoForm />
+      </div>
+    );
+  }
+}
 
 export default GeneralInfo;
