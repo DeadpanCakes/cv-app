21f2c003c981f178b60a5236ae093667f2d15254
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-02-19 13:33:18 -0800

    made WorkExperience.js to accept, store, and display work experince information

---
+++ src/Components/WorkExperience.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class WorkExperience extends Component {
+    constructor() {
+        super()
+        this.state = {
+            workHistory: []
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Work Experience</h2>
+            </div>
+        )
+    }
+}
+
+export default WorkExperience
