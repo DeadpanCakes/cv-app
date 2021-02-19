a8a06daedf1e7a834b62a5d810c1c453f701a78e
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-02-19 13:50:41 -0800

    added displayHistory method to WorkExperience.js, some placeholder objects, and imported into App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import PersonalInfo from './Components/PersonalInfo'
+import WorkExperience from './Components/WorkExperience'
 
 function App() {
   return (
     <div className="App">
         <PersonalInfo />
+        <WorkExperience />
     </div>
   );
 }
--- src/Components/WorkExperience.js
+++ src/Components/WorkExperience.js
@@ -1,20 +1,51 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class WorkExperience extends Component {
-    constructor() {
-        super()
-        this.state = {
-            workHistory: []
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      workHistory: [
+        {
+          key: 0,
+          companyName: "Champs",
+          position: "Sales",
+          timeWorked: "2019-2020",
+          desc: "Sold things",
+        },
+        {
+          key: 1,
+          companyName: "McDonald's",
+          position: "Sanitary Worker",
+          timeWorked: "2018-2019",
+          desc: "Cleaned things",
+        },
+      ],
+    };
+    this.displayHistory = this.displayHistory.bind(this);
+  }
 
-    render() {
-        return (
-            <div>
-                <h2>Work Experience</h2>
-            </div>
-        )
-    }
+  displayHistory(arr) {
+    return arr.map((job) => {
+      const { companyName, position, timeWorked, desc } = job;
+      return (
+        <div>
+          <h1>{companyName}</h1>
+          <h2>{position}</h2>
+          <h2>{timeWorked}</h2>
+          <h2>{desc}</h2>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Work Experience</h2>
+        {this.displayHistory(this.state.workHistory)}
+      </div>
+    );
+  }
 }
 
-export default WorkExperience
+export default WorkExperience;
