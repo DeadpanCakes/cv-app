c6f30371af35f0493846285f10c0fc39e0cb7b08
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-02-19 14:27:50 -0800

    wrote handleSubmit to return an object based on data in form's state

---
+++ src/Components/WorkExperienceForm.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+
+class WorkExperienceForm extends Component {
+    constructor() {
+        super()
+        this.state = {
+            companyName: "",
+            position: "",
+            timeWorked: "",
+            desc: "",
+        }
+        this.handleInput = this.handleInput.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    
+    handleInput(e) {
+        const { name, value} = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(this.state)
+        return this.state
+    }
+
+    render() {
+        const {companyName, position, timeWorked, desc} = this.state
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input name="companyName" value={companyName} onChange={this.handleInput} placeholder="Name Of Company"></input>
+                <input name="position" value={position} onChange={this.handleInput} placeholder="Position"></input>
+                <input name="timeWorked" value={timeWorked} onChange={this.handleInput} placeholder="Time Worked"></input>
+                <input name="desc" type="textarea" value={desc} onChange={this.handleInput} placeholder="Briefly Describe Your Responsibilites During Your Time Working This Job"></input>
+                <button>Submit</button>
+            </form>
+        )
+    }
+}
+
+export default WorkExperienceForm
