6343efe2750cf0ac10e6cd029eec0a61b67b1ef4
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-02-22 17:35:55 -0800

    made an EducationInfo component, to mirror WorkExperienceInfo

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -1,30 +1,12 @@
 import React, { Component } from "react";
 import EducationForm from "./EducationForm";
+import EducationInfo from "./EducationInfo";
 
 class Education extends Component {
   constructor() {
     super();
     this.state = {
-      educationHistory: [
-        {
-          key: 0,
-          institution: "College",
-          duration: "2010-2014",
-          isBeingEdited: false,
-        },
-        {
-          key: 1,
-          institution: "School",
-          duration: "2017-2019",
-          isBeingEdited: false,
-        },
-        {
-          key: 2,
-          institution: "Place",
-          duration: "1930-1933",
-          isBeingEdited: false,
-        },
-      ],
+      educationHistory: [],
     };
     this.addTerm = this.addTerm.bind(this);
     this.displayHistory = this.displayHistory.bind(this);
@@ -43,39 +25,39 @@ class Education extends Component {
     const updatedHistory = this.state.educationHistory.filter(
       (term) => term.key !== key
     );
-    console.log(updatedHistory);
     this.setState({ educationHistory: updatedHistory });
   }
 
   editTerm(key) {
-    const targetTerm = this.state.educationHistory.find(item => item.key === key)
-    const updatedHistory = this.state.educationHistory.map(term=> {
-        if (term===targetTerm) {
-          return {
-            ...targetTerm,
-            isBeingEdited: true,
-          }
-        }
-        return term
-      })
+    const targetTerm = this.state.educationHistory.find(
+      (item) => item.key === key
+    );
+    const updatedHistory = this.state.educationHistory.map((term) => {
+      if (term === targetTerm) {
+        return {
+          ...targetTerm,
+          isBeingEdited: true,
+        };
+      }
+      return term;
+    });
     this.setState({
-      educationHistory: updatedHistory
-    })
-    console.log(this.state)
+      educationHistory: updatedHistory,
+    });
   }
 
   submitTermEdit(editedTerm) {
-    const targetKey = editedTerm.key
-    const updatedHistory = this.state.educationHistory.map(term => {
+    const targetKey = editedTerm.key;
+    const updatedHistory = this.state.educationHistory.map((term) => {
       if (term.key === targetKey) {
-        return editedTerm
+        return editedTerm;
       } else {
-        return term
+        return term;
       }
-    })
+    });
     this.setState({
-      educationHistory: updatedHistory
-    })
+      educationHistory: updatedHistory,
+    });
   }
 
   displayHistory() {
@@ -87,29 +69,34 @@ class Education extends Component {
           handleSubmit={this.submitTermEdit}
         />
       ) : (
-        <li key={term.key}>
-          <h3>{term.institution}</h3>
-          <p>{term.duration}</p>
-          <button onClick={() => this.editTerm(term.key)}>Edit</button>
-          <button onClick={() => this.removeTerm(term.key)}>Delete</button>
-        </li>
+        <EducationInfo
+          key={term.key}
+          term={term}
+          edit={this.editTerm}
+          remove={this.removeTerm}
+        />
       );
     });
   }
 
   toggleForm() {
-    const form = document.getElementById("educationForm")
-    form.style.display !== 'none' ? form.style.display='none' : form.style.display='flex';
+    const form = document.getElementById("educationForm");
+    form.style.display !== "none"
+      ? (form.style.display = "none")
+      : (form.style.display = "flex");
   }
 
   render() {
     return (
       <div>
         <h2>Education</h2>
-        <ul>
-        {this.displayHistory()}
-        </ul>
-        <EducationForm info={this.state} handleSubmit={this.addTerm} id="educationForm" style={{display: 'none'}}/>
+        <ul>{this.displayHistory()}</ul>
+        <EducationForm
+          info={this.state}
+          handleSubmit={this.addTerm}
+          id="educationForm"
+          style={{ display: "none" }}
+        />
         <button onClick={this.toggleForm}>Add/Hide</button>
       </div>
     );
---
+++ src/Components/EducationInfo.js
@@ -0,0 +1,13 @@
+const EducationInfo = (props) => {
+  const { key, institution, duration } = props.term;
+  return (
+    <li key={key}>
+      <h3>{institution}</h3>
+      <p>{duration}</p>
+      <button onClick={() => props.edit(key)}>Edit</button>
+      <button onClick={() => props.remove(key)}>Delete</button>
+    </li>
+  );
+};
+
+export default EducationInfo;
